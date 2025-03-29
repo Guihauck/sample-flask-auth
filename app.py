25a503334f2317409b65f35fef6551290a7c0d02
Guihauck <guihauck1998@gmail.com>
2025-03-29 20:08:35 -0300

    Creating logout and user registration views

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key" # Utilizado pelo Flask para proteger as informações armazenadas.
@@ -31,6 +31,27 @@ def login():
       return jsonify({"message":"autenticação realizada com sucesso!"})
     
   return jsonify({"message":"Desculpe, não autenticado, login ou senha incorretos!"}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required # Verifica se o login já está autenticado.
+def logout():
+  logout_user()
+  return jsonify({"message": "Logout realizado com êxito!"})
+
+@app.route('/user', methods=['POST'])
+def create_user():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+  adressemail = data.get("adressemail")
+
+  if username and password:
+    user = User(username=username, password=password, adressemail=adressemail)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message": "Usuário cadastrado com sucesso!"})
+  
+  return jsonify({"message": "Cadastro incorreto, verifique os dados cadastrados."}),400
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
